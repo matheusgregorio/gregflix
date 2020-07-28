d386a7a0f2ceb47d444c6e5bff68d1bca8948bf5
Matheus Gregorio <cxlmo7@gmail.com>
2020-07-27 22:56:32 -0300

    initial commit
    Primeiro commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      {/* <a href="https://www.alura.com.br/">
+        <img src="../assets/img/Logo.png" alt="Logo Gregflix" />
+      </a> */}
+      <p>
+         Criado durante a
+        
+        
+          Imersão React da Alura
+        
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
